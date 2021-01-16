713ee368467e21ecec2290ec6355a4edf72d0603
Philipp Gatzka <philipp.gatzka@gmx.ch>
2021-01-16 22:14:03 +0100

    Added the feature to view the full entry on the main screen of the Application.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,8 +1,17 @@
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 public class Controller {
     public ListView lv_entries;
+    public Label l_firstName;
+    public Label l_lastName;
+    public Label l_street;
+    public Label l_city;
+    public Label l_zip;
+    public Label l_email;
     private Model model;
 
     public void setMyModel(Model model) {
@@ -14,6 +23,16 @@ public class Controller {
             e.printStackTrace();
         }
         lv_entries.setItems(model.entries);
+        lv_entries.getSelectionModel().selectedItemProperty().addListener((observableValue, oldEntry, newEntry) -> showEntry((Entry) newEntry));
+    }
+
+    public void showEntry(Entry entry) {
+        l_firstName.setText(entry.getFirstName());
+        l_lastName.setText(entry.getLastName());
+        l_city.setText(entry.getCity());
+        l_email.setText(entry.getEmail());
+        l_street.setText(entry.getStreet());
+        l_zip.setText(entry.getZip() + "");
     }
 
     public void saveAdd(ActionEvent actionEvent) {
@@ -22,16 +41,13 @@ public class Controller {
 
     public void cancel(ActionEvent actionEvent) {
         System.out.println("Not Implemented yet!");
-
     }
 
     public void saveEdit(ActionEvent actionEvent) {
         System.out.println("Not Implemented yet!");
-
     }
 
     public void delete(ActionEvent actionEvent) {
         System.out.println("Not Implemented yet!");
-
     }
 }
