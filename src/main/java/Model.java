27f6c56863630a5bb180870d4df12964c09421d3
Philipp Gatzka <philipp.gatzka@gmx.ch>
2021-01-16 21:21:03 +0100

    Clean Application Push

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -10,44 +10,4 @@ import java.io.*;
 import java.util.Comparator;
 
 public class Model {
-    ObservableList<Entry> entries = FXCollections.observableArrayList();
-
-    public Model() throws Exception {
-        loadData();
-        entries.sort(Entry::compareTo);
-    }
-
-    private void saveData() throws Exception {
-        entries.sort(Entry::compareTo);
-        JSONArray array = new JSONArray();
-        for (Entry entry : entries) {
-            array.add(entry.toJson());
-        }
-        BufferedWriter writer = new BufferedWriter(new FileWriter(new File("save.json")));
-        writer.write(array.toJSONString());
-        writer.close();
-
-    }
-
-    private void loadData() throws Exception {
-        String filePath = "save.json";
-        BufferedReader reader = new BufferedReader(new FileReader(new File(filePath)));
-        StringBuilder builder = new StringBuilder();
-        String line = reader.readLine();
-        while (line != null) {
-            builder.append(line);
-            line = reader.readLine();
-        }
-        JSONParser parser = new JSONParser();
-        JSONArray array = (JSONArray) parser.parse(builder.toString());
-        array.forEach(o -> {
-            entries.removeAll();
-
-            entries.add(Entry.fromJson((JSONObject) o));
-
-        });
-
-    }
-
-
 }
